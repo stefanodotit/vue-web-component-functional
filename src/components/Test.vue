3fe0d5da918527a0a98037fdc72ac502034a39dd
Stefano Tagliabue <stefano.tagliabue@musement.com>
2019-05-03 17:19:18 +0200

    init

---
+++ src/components/Test.vue
@@ -0,0 +1,13 @@
+<script>
+export default {
+    name: 'Test',
+    functional: true,
+    render: (createElement, context) => {
+        return createElement('div')
+    }
+}
+</script>
+
+<style>
+    .test{}
+</style>
